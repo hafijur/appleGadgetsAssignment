b1e844dcad0294bb1805120baa8e3e011b26faad
Md Hafijur Rahman <hafijur300000@gmail.com>
2025-01-21 12:08:08 +0600

    module seeder added

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -20,6 +20,32 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        // Call all module seeders
+        $this->seedModuleSeeder();
+    }
 
+    private function seedModuleSeeder()
+    {
+        // Path to modules_statuses.json
+        $moduleStatusesPath = base_path('modules_statuses.json');
+
+        if (file_exists($moduleStatusesPath)) {
+            $moduleStatuses = json_decode(file_get_contents($moduleStatusesPath), true);
+
+            foreach ($moduleStatuses as $moduleName => $isEnabled) {
+                if ($isEnabled) {
+                    $moduleSeeder = "Modules\\$moduleName\\Database\\Seeders\\{$moduleName}DatabaseSeeder";
+
+                    if (class_exists($moduleSeeder)) {
+                        $this->call($moduleSeeder);
+                        $this->command->info("Seeded: $moduleSeeder");
+                    } else {
+                        $this->command->warn("Seeder not found for module: $moduleName");
+                    }
+                }
+            }
+        } else {
+            $this->command->error('modules_statuses.json file not found!');
+        }
     }
 }
