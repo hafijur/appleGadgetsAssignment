0c5bc02b03205293d02455c52df4994f3087b8f1
Md Hafijur Rahman <hafijur300000@gmail.com>
2025-01-22 17:03:12 +0600

    utils added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,9 +7,7 @@ import './index.css';
 
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
-
-
-
+import { getCurrentDate } from './utils/dataUtils'; './utils/dataUtils';
 
 const app = createApp(App);
 app.use(VueSweetalert2);
@@ -19,13 +17,7 @@ app.use(router);
 
 app.mixin({
     methods: {
-        getCurrentDate() {
-            const date = new Date();
-            const year = date.getFullYear();
-            const month = String(date.getMonth() + 1).padStart(2, '0');
-            const day = String(date.getDate()).padStart(2, '0');
-            return `${year}-${month}-${day}`;
-        },
+        getCurrentDate,
     },
 });
 
